aa73cfc5f257ffde3a58438565daab60d95892cb
Robert Edmonds <edmonds@users.noreply.github.com>
2023-12-27 01:11:24 -0500

    setupRealtimePriority(): Apply SCHED_FIFO priority to all threads
    Not really all, but almost all. This just sets the default for
    pthread_create() with a NULL `attr` argument. Unfortunately
    pthread_setattr_default_np() is nonportable.
    
    If ffmpeg is built with --enable-pocketsphinx (like on Debian unstable
    right now) it will pull in libopenblas which sneaks an entire thread
    pool into the process via an __attribute__((constructor)) function. See
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1059505. But vacon
    also doesn't care about the priority of threads in some indirect
    dependency that we're never going to use.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <stdexcept>
 #include <utility>
 
+#include <pthread.h>
 #include <sched.h>
 
 using std::string;
@@ -18,37 +19,66 @@ int video_receiver(const string&);
 
 volatile std::sig_atomic_t gSignalUSR1;
 
-void signalHandler(int signal) {
+static void signalHandler(int signal) {
 	if (signal == SIGUSR1) {
 		gSignalUSR1 = 1;
 	}
 }
 
-void setupRealtimePriority()
+static bool setupRealtimePriority()
 {
-	int prio = sched_get_priority_min(SCHED_RR);
-	if (prio == -1) {
+	const int min_fifo_prio = sched_get_priority_min(SCHED_FIFO);
+	if (min_fifo_prio == -1) {
 		std::cerr
-			<< "Error: sched_get_priority_min() failed: "
+			<< "sched_get_priority_min() failed: "
 			<< std::strerror(errno)
 			<< std::endl;
-		return;
+		return false;
 	}
 
-	struct sched_param param = {
-		.sched_priority = prio + 1,
+	const struct sched_param param = {
+		.sched_priority = min_fifo_prio + 1,
 	};
 
-	int ret = sched_setscheduler(0, SCHED_RR | SCHED_RESET_ON_FORK, &param);
-	if (ret != 0) {
+	if (sched_setscheduler(0, SCHED_FIFO, &param) != 0) {
 		std::cerr
-			<< "Error: sched_setscheduler() failed: "
+			<< "pthread_setschedparam() failed: "
 			<< std::strerror(errno)
 			<< std::endl;
+		return false;
 	}
+
+	static pthread_attr_t attr;
+	if (pthread_attr_init(&attr)) {
+		std::cerr << "pthread_attr_init() failed!" << std::endl;
+		return false;
+	}
+
+	if (pthread_attr_setschedpolicy(&attr, SCHED_FIFO)) {
+		std::cerr << "pthread_attr_setschedpolicy() failed!" << std::endl;
+		return false;
+	}
+
+	if (pthread_attr_setschedparam(&attr, &param)) {
+		std::cerr << "pthread_attr_setschedparam() failed!" << std::endl;
+		return false;
+	}
+
+	if (pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED)) {
+		std::cerr << "pthread_attr_setinheritsched() failed!" << std::endl;
+		return false;
+	}
+
+	if (pthread_setattr_default_np(&attr)) {
+		std::cerr << "pthread_setattr_default_np() failed!" << std::endl;
+		return false;
+	}
+
+	return true;
 }
 
-int main(int argc, char **argv) try {
+int main(int argc, char **argv) try
+{
 	setupRealtimePriority();
 
 	std::signal(SIGUSR1, signalHandler);
